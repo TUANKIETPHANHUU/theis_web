6e8fcd9003347b3a57dc71acd21b205c394dd096
Phan Huu Tuan Kiet <117705056+TUANKIETPHANHUU@users.noreply.github.com>
2026-04-01 07:37:19 +0000

    1

--- KPDL-main/app.py
+++ KPDL-main/app.py
@@ -13,12 +13,12 @@ scaler = joblib.load(os.path.join(MODEL_DIR, 'scaler.pkl'))
 selected_features = joblib.load(os.path.join(MODEL_DIR, 'selected_features.pkl'))
 
 # Input
-age = st.number_input("Age", 0, 120, 25, step=1)
-gender = st.number_input("Gender", 0, 1, 0, step=1)
-bp = st.number_input("BloodPressure", 0, 300, 120, step=1)
-chol = st.number_input("Cholesterol", 0, 600, 200, step=1)
-hr = st.number_input("HeartRate", 0, 300, 70, step=1)
-qpf = st.number_input("QuantumPatternFeature", 0, 100, 1, step=1)
+age = st.number_input("Age")
+gender = st.number_input("Gender")
+bp = st.number_input("BloodPressure")
+chol = st.number_input("Cholesterol")
+hr = st.number_input("HeartRate")
+qpf = st.number_input("QuantumPatternFeature")
 
 if st.button("Dự đoán"):
     df = pd.DataFrame([{
@@ -43,4 +43,4 @@ if st.button("Dự đoán"):
     labels = {0: 'Không bệnh', 1: 'Nhẹ', 2: 'Trung bình', 3: 'Nặng'}
 
     st.success(f"Kết quả: {labels[pred]}")
-    st.write("Xác suất:", np.round(proba, 3))
+    st.write("Xác suất:", proba)
